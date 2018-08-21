080ce5f9dde9d925e99f19ee0fca03d7e8710c47
Sonny Yang <klzsysy@gmail.com>
2018-08-21 15:28:59 +0800

    fix  路径兼容性2

--- Text_Crawl.py
+++ Text_Crawl.py
@@ -277,7 +277,7 @@ class FeaturesList(object):
             filename_format = os.path.join('.', self.down_path, '{0:<{1}} {2}.txt'.format(str(count), len(str(page_count)), title))
             # filename_format = '.\{0}\{1:<{2}} {3}.txt'.format(self.down_path, str(count), len(str(page_count)), title)
 
-        saved_filename.append(filename_format.split('\\')[-1])                      # 记录文件名
+        saved_filename.append(os.path.split(filename_format)[-1])                      # 记录文件名
         if self.args.s:
             # 单页模式缓存文本内容 #并记录文件名
             # saved_filename.append(filename_format.split('\\')[-1])
