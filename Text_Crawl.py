2b8b008ddf860e42ceeb78ed2f8ea1827adb060f
Sonny Yang <klzsysy@gmail.com>
2018-08-21 15:11:23 +0800

    fix  路径兼容性

--- Text_Crawl.py
+++ Text_Crawl.py
@@ -271,9 +271,11 @@ class FeaturesList(object):
             return True
         title = re.sub(r'[/\\:\*\?\"<>\|]', '-', title)                             # 过滤文件名非法字符
         if self.args.s and count == 1000 and self.args.direction is False:
-            filename_format = '.\{}\{}.txt'.format(self.down_path, title)
+            filename_format = os.path.join('.', self.down_path, title + '.txt')
+            # filename_format = '.\{}\{}.txt'.format(self.down_path, title)
         else:
-            filename_format = '.\{0}\{1:<{2}} {3}.txt'.format(self.down_path, str(count), len(str(page_count)), title)
+            filename_format = os.path.join('.', self.down_path, '{0:<{1}} {2}.txt'.format(str(count), len(str(page_count)), title))
+            # filename_format = '.\{0}\{1:<{2}} {3}.txt'.format(self.down_path, str(count), len(str(page_count)), title)
 
         saved_filename.append(filename_format.split('\\')[-1])                      # 记录文件名
         if self.args.s:
